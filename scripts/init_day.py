2ec61cc16fc56bdfd6e8c8bd4e7c7ce240b2b816
Erik Karlgren Domercq <erik.karlgren@gmail.com>
2022-12-04 16:58:59 +0100

    Day 3: started & made changes to scripts

--- scripts/init_day.py
+++ scripts/init_day.py
@@ -10,19 +10,22 @@ def main():
     day_format = f"day{str(args.day).zfill(2)}"
     tests_folder = f"tests/{day_format}"
 
+    # Create tests folder & add day's input
     subprocess.run(["mkdir", "-p", tests_folder])
     subprocess.run(["aocdl", "-year", "2022", "-day", str(args.day)])
     subprocess.run(["mv", "-f", "input.txt", tests_folder])
+    subprocess.run(["git", "add", tests_folder])
 
+    # Create files for part 1 and 2
     for file in [f"src/{day_format}_a.rs", f"src/{day_format}_b.rs"]:
         with open(file, "w") as f:
             f.write(
                 inspect.cleandoc(
                     """
-            use std::io;
+            use std::io::{self, BufRead};
 
             pub fn solve() -> io::Result<()> {
-                for line in io::stdin().lock().lines().map(|l| l.unwrap){
+                for line in io::stdin().lock().lines().map(|l| l.unwrap()){
                     // Here's your code
                 }
 
@@ -31,13 +34,15 @@ def main():
             """
                 )
             )
+        subprocess.run(["git", "add", file])
 
+    # Update main file
     main_file = open("src/main.rs", "r").read()
     main_file = f"mod {day_format}_a;\nmod {day_format}_b;\n" + main_file
     main_file = main_file.replace(
-        "    match problem {\n",
+        "    match args.problem.as_str() {\n",
         (
-            "    match problem {\n"
+            "    match args.problem.as_str() {\n"
             f'        "{day_format}_a" => {day_format}_a::solve().unwrap(),\n'
             f'        "{day_format}_b" => {day_format}_b::solve().unwrap(),\n'
         ),
@@ -45,6 +50,7 @@ def main():
 
     with open("src/main.rs", "w") as f:
         f.write(main_file)
+    subprocess.run(["git", "add", main_file])
 
 
 def parse_args():
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
-mod day02_b;
+mod day03_a;
+mod day03_b;
 mod day01_b;
+mod day02_b;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    problem: String,
+}
 
 fn main() {
-    let problem = "";
-    match problem {
+    let args = Args::parse();
+
+    match args.problem.as_str() {
+        "day03_a" => day03_a::solve().unwrap(),
+        "day03_b" => day03_b::solve().unwrap(),
         "day02_b" => day02_b::solve().unwrap(),
         "day01_b" => day01_b::solve().unwrap(),
         _ => unimplemented!("Unimplemented or not a problem"),
