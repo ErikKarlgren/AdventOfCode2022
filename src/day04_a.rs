d981b652cbe874ac0694277ac07f8be1808badd2
Erik Karlgren Domercq <erik.karlgren@gmail.com>
2022-12-06 02:34:57 +0100

    Day 4: passed part 1

--- src/day04_a.rs
+++ src/day04_a.rs
@@ -1,9 +1,57 @@
-use std::io::{self, BufRead};
+use std::{
+    io::{self, BufRead},
+    ops::Range,
+};
+
+use itertools::Itertools;
 
 pub fn solve() -> io::Result<()> {
-    for line in io::stdin().lock().lines().map(|l| l.unwrap()){
-        // Here's your code
+    let mut sum = 0;
+    for line in io::stdin().lock().lines().map(|l| l.unwrap()) {
+        let (first, second) = parse_line(line);
+        if one_fully_contains_the_other(first, second) {
+            sum += 1;
+        }
     }
 
+    print!("{}", sum);
+
     Ok(())
+}
+
+fn parse_line(line: String) -> (Range<i32>, Range<i32>) {
+    line.trim()
+        .split(',')
+        .map(|pair| {
+            let (min, max) = pair
+                .split('-')
+                .map(|s| s.parse().expect("Not a number"))
+                .collect_tuple()
+                .expect("Range does not have 2 elems (min and max)");
+            min..max
+        })
+        .collect_tuple()
+        .expect("Expected to find 2 elfs")
+}
+
+fn one_fully_contains_the_other(first: Range<i32>, second: Range<i32>) -> bool {
+    first.start <= second.start && second.end <= first.end
+        || second.start <= first.start && first.end <= second.end
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_parse_line() {
+        assert_eq!(parse_line("12-34,56-78".to_string()), (12..34, 56..78));
+    }
+
+    #[test]
+    fn test_fully_contains() {
+        assert!(one_fully_contains_the_other(0..10, 3..6));
+        assert!(one_fully_contains_the_other(6..6, 4..6));
+        assert!(!one_fully_contains_the_other(0..5, 4..10));
+    }
 }
